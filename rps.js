eb8866c984e6008a1b92b1400a03fa2b99d74623
alexahn1929 <121832249+alexahn1929@users.noreply.github.com>
2023-04-13 23:13:08 -0400

    Write specifications for game
    Create homepage and basic specs for a rock-paper-scissors game.

---
+++ rps.js
@@ -0,0 +1,27 @@
+/**
+ * Randomly select rock, paper, or scissors.
+ * @returns {string} a string containing either rock, paper, or scissors
+ */
+function getComputerChoice() {
+    
+}
+
+/**
+ * Determines the result of a rock-paper-scissors game based on the player and computer selections.
+ * @param {string} playerSelection
+ * @param {string} computerSelection 
+ * @returns {string} a string saying whether the player won, lost, or tied
+ */
+function playRound(playerSelection, computerSelection) {
+
+}
+
+/**
+ * Play rock-paper-scissors games until either the player or computer has won five games. Print the results of each round and of the game overall to the console.
+ * @returns {void}
+ */
+function game() {
+
+}
+
+game();
